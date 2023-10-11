d23812e9e6ac894d1141553b0f0452c411002260
Invius <marco.fernandes.inf10@hotmail.com>
2023-10-11 01:32:50 +0100

    contacts page with real data

--- src/Pages/Contacts/Contacts.tsx
+++ src/Pages/Contacts/Contacts.tsx
@@ -1,32 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { IConfigResult } from "../../Models/ConfigResult";
+import { configService } from "../../Services/Config.service";
 import styles from "./Contacts.module.scss";
+import Loading from "../../Components/Loading/Loading";
 
 export default function Contacts() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["pord"],
+    queryFn: (): Promise<IConfigResult> => configService.contactsPage(),
+  });
+
   return (
     <div className={styles.container}>
-      <div>
-        {`
-                Horários
-                Seg:
-                Ter:
-                Qua:
-                Qui:
-                Sex:
-                Contacto
-                Rua do buragal nº 218 Aradas-Aveiro
-                3810-382
-                TLF: 234427229
-                TLM:963928334
-                hsilva.maria@gmail.com
-                `
-          .split("\n")
-          .map((element, index) => {
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div>
+          {data?.config.split("\n").map((element, index) => {
             return (
               <p className={styles.data} key={index}>
                 {element}
               </p>
             );
           })}
-      </div>
+        </div>
+      )}
       <img
         className={styles.store}
         src="./Contacts/contacts.jpg"
