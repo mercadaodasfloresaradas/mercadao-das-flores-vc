05350f04885188e6a42a969245b85715268ef876
Invius <marco.fernandes.inf10@hotmail.com>
2024-01-09 00:02:59 +0000

    try to prevent darkmode and aligment of contacts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes";
 import "./App.scss";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Loading from "./Components/Loading/Loading";
 import CheckBrowser from "./Components/CheckBrowser/CheckBrowser";
